fd6a599400d266f7744c91af3527238e44fd1e5e
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-03-04 22:15:20 +0100

    add a function that calculates all legal next positions of the knight

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
-const chessBoard = ([0,1,2,3,4,5,6,7,8],[0,1,2,3,4,5,6,7,8])
-
+const chessBoard = [[0,1,2,3,4,5,6,7,8],[0,1,2,3,4,5,6,7,8]]
+// ([x-axis],[y-axis])
 
 let positionKnight = [1,2]
 let positionStart = [0,0]
 
-function knightMoves(positionStart, positionKnight){
-
-}
-
 // we need a breadth first algorithm
 // this makes sure we check the closest fields first
-function calculateMoveTree(position, goal){
-    // x-axis moves: +2,+1 +2,-1 -2,+1 -2,-1
-    // y-axis moves: +1,+2 +1,-2 -1,+2 -1,-2
 
- 
-    let move1 = position + [2,1]
+    // exclude the move that takes us back to position
+
     // make sure it is a legal move (within the field)
-    if(chessBoard.includes(move1)){
-        if(position === goal){
-            // we have reached our goal
-            return tree
+    // if(chessBoard.includes(move1)){
+    //     if(position === goal){
+    //         // we have reached our goal
+    //         return tree // how??
+    //     }
+    //     else{
+    //         // keep going further
+    //         calculateMoveTree(move1, goal)
+    //     }
+    // }
+
+function calculateNextPositions(position){
+    // x-axis moves: 2,1 2,-1 -2,1 -2,-1
+    // y-axis moves: 1,2 1,-2 -1,2 -1,-2
+    let possibleMoves = [[1,2],[1,-2],[-1,2],[-1,-2],[2,1],[2,-1],[-2,1],[-2,-1]]
+    let possiblePositions = []
+
+    possibleMoves.forEach(move => {
+        let newPosition = [position[0]+move[0], position[1] + move[1]]
+        // check if this newPosition is legal
+        if(chessBoard[0].includes(newPosition[0]) && chessBoard[1].includes(newPosition[1])){
+            possiblePositions.push(newPosition)
         }
         else{
-            // keep going further
-            calculateMoveTree(move1, goal)
+            // this position is not on the board
         }
-    }
-
-
-
-
-
-
+    });
+    return possiblePositions
 }
+
+calculateNextPositions([0,0], [1,2])
