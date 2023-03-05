cd2e1443d471aded80439e4fab2154e943ed1cc4
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-03-05 15:32:23 +0100

    add a function that adds a knight image to the correct place in the grid

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const chessBoard = [[0,1,2,3,4,5,6,7,8],[0,1,2,3,4,5,6,7,8]]
+const chessBoard = [[0,1,2,3,4,5,6,7],[0,1,2,3,4,5,6,7]]
 // ([x-axis],[y-axis])
 
-let positionGoal = [0,4]
+let positionGoal = [3,7]
 let positionStart = [0,0]
 
 // we need a breadth first algorithm
@@ -49,3 +49,24 @@ function calculateNextPositions(position){
     });
     return possiblePositions
 }
+
+function animateKnightsMovement(position){
+
+    let x = position[0]
+    let y = position[1]
+    
+    let gridId = x + 64-(y*8)-7
+// equation for grid id: x + 64-(y*8)-7
+// 0 + 64-(0*8)-7 = 57
+// 1 + 64-(1*8)-7 = 50
+// 2 + 64-(2*8)-7 = 43
+let goalGridItem = document.querySelector(`#f${gridId}`) 
+
+    let knightImage = document.createElement("img")
+    knightImage.src = "chess.png"
+    knightImage.id = "knight"
+    goalGridItem.append(knightImage)
+}
+
+let testArray = [1,1]
+animateKnightsMovement(testArray)
